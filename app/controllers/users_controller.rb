a087ad093f30543d6203c6d60a5ec25ecdf59c18
VoilinYu <mutesiren@icloud.com>
2019-07-27 16:50:39 +0800

    Implement basic login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,8 +9,8 @@ class UsersController < ApplicationController
 
   def create
     @user = User.new(user_params)
-
     if @user.save
+      log_in @user
       flash[:success] = "Welcome to the Microposts App"
       redirect_to user_url(@user)
     else
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+
   root 'static_pages#home'
 
   get 'help',     to: 'static_pages#help'
@@ -8,6 +9,11 @@ Rails.application.routes.draw do
   get '/contact', to: 'static_pages#contact'
 
   get '/signup',  to: 'users#new'
-  
+
+  get '/login',   to: 'sessions#new'
+
+  post '/login',   to: 'sessions#create'
+
+  delete 'logout',   to: 'sessions#destroy'
   resources :users
 end
