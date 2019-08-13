d34042e3c04d660351b455ceb81716e825afb4a4
VoilinYu <mutesiren@icloud.com>
2019-08-13 11:55:06 +0800

    Add user following

--- config/application.rb
+++ config/application.rb
@@ -2,6 +2,7 @@ require_relative 'boot'
 
 require 'rails/all'
 
+require File.expand_path('../boot', __FILE__)
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -11,6 +12,9 @@ module MicropostsApp
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
+
+    # 在使用 Ajax 处理的表单中添加真伪令牌
+    config.action_view.embed_authenticity_token_in_remote_forms = true
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
